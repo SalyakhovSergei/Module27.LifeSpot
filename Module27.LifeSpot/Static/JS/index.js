5e1241071ed8bd40d5c3cb4215cfe207cc88964e
Sergey <salyakhovsergei@gmail.com>
2021-09-10 14:46:27 +1000

    29.5

--- Module27.LifeSpot/Static/JS/index.js
+++ Module27.LifeSpot/Static/JS/index.js
@@ -1,11 +1,49 @@
-﻿let age = prompt("Пожалуйста, введите ваш возраст");
+﻿function handleSession()
+{
+    let session =  new Map();
+    session.set("userAgent", window.navigator.userAgent)
+    
+    session.set("age", prompt("Пожалуйста, введите ваш возраст"))
+    
+    if(session.get("age") >= 18)
+    {
+        let startDate = new Date().toLocaleString();
 
-if(age >= 18)
-{
-    alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
+        alert("Приветствуем на LifeSpot! " + '\n' +  "Текущее время: " + startDate );
+        session.set("startDate", startDate)
+    }
+    else
+    {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+    
+    return session;
 }
-else
+
+let sessionLog = function logSession(session) 
 {
-    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-    window.location.href = "http://www.google.com"
+    for (let result of session)
+    {
+        console.log(result)
+    }
+}
+
+
+function filterContent(inputParseFunction)
+{
+    let elements = document.getElementsByClassName('video-container');
+    
+    for (let i = 0; i <= elements.length; i++ )
+    {
+        let videoText = elements[i].getElementsByTagName('h3')[0].innerText;
+        
+        if(!videoText.toLowerCase().includes(inputParseFunction().toLowerCase()))
+        {
+            elements[i].style.display = 'none';
+        } else 
+        {
+            elements[i].style.display = 'inline-block';
+        }
+    }
 }
