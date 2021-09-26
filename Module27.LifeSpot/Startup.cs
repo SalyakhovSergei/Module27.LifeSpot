3fb9b83498d0984250c1736aa00c1992e4c159ba
Саляхов Сергей <salyakhovsergei@gmail.com>
2021-09-26 14:47:06 +1000

    1

--- Module27.LifeSpot/Startup.cs
+++ Module27.LifeSpot/Startup.cs
@@ -3,6 +3,7 @@ using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing.Matching;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -20,74 +21,13 @@ namespace Module27.LifeSpot
                app.UseDeveloperExceptionPage();
  
            app.UseRouting();
-           
-           string footerHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "footer.html"));
-           string sideBarHtml =  File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "sideBar.html"));
- 
+
            app.UseEndpoints(endpoints =>
            {
-               endpoints.MapGet("/", async context =>
-               {
-                   var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html");
-                   
-                   var html =  new StringBuilder(await File.ReadAllTextAsync(viewPath))
-                       .Replace("<!--SIDEBAR-->", sideBarHtml)
-                       .Replace("<!--FOOTER-->", footerHtml);
-                  
-                   await context.Response.WriteAsync(html.ToString());
-               });
-              
-               endpoints.MapGet("/testing", async context =>
-               {
-                   var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "testing.html");
-                  
-                   var html =  new StringBuilder(await File.ReadAllTextAsync(viewPath))
-                       .Replace("<!--SIDEBAR-->", sideBarHtml)
-                       .Replace("<!--FOOTER-->", footerHtml);
-                  
-                   await context.Response.WriteAsync(html.ToString());
-               });
-               
-               endpoints.MapGet("/about", async context =>
-               {
-                   var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "about.html");
-  
-                   var html =  new StringBuilder(await File.ReadAllTextAsync(viewPath))
-                       .Replace("<!--SIDEBAR-->", sideBarHtml)
-                       .Replace("<!--FOOTER-->", footerHtml);
-  
-                   await context.Response.WriteAsync(html.ToString());
-               });
-              
-               endpoints.MapGet("/Static/CSS/index.css", async context =>
-               {
-                   var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", "index.css");
-                   var css = await File.ReadAllTextAsync(cssPath);
-                   await context.Response.WriteAsync(css);
-               });
-              
-               endpoints.MapGet("/Static/JS/index.js", async context =>
-               {
-                   var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "index.js");
-                   var js = await File.ReadAllTextAsync(jsPath);
-                   await context.Response.WriteAsync(js);
-               });
-               
-               endpoints.MapGet("/Static/JS/testingJS.js", async context =>
-               {
-                   var jsPathForTesting = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "testingJS.js");
-                   var jsTesting = await File.ReadAllTextAsync(jsPathForTesting);
-                   await context.Response.WriteAsync(jsTesting);
-               });
-               
-               endpoints.MapGet("/Static/JS/about.js", async context =>
-               {
-                   var jsPathForAbout = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "about.js");
-                   var jsAbout = await File.ReadAllTextAsync(jsPathForAbout);
-                   await context.Response.WriteAsync(jsAbout);
-               });
-               
-               
+               endpoints.MapCss();
+               endpoints.MapJs();
+               endpoints.MapHtml();
+               endpoints.MapImg();
            });
        }
    }
--- Module27.LifeSpot/Static/JS/about.js
+++ Module27.LifeSpot/Static/JS/about.js
@@ -1,6 +1,4 @@
-﻿alert("Javascript для страницы 'о проекте' подключен!")
-
-function getReview() {
+﻿function getReview() {
 
     let review = {}
 
@@ -84,4 +82,46 @@ function addLike(id) {
     
     element.innerText = array.join(' ')
 }
+
+//var slides = document.querySelectorAll('#slides .slide')
+//var currentSlide = 0;
+//
+//function nextSlide() {
+//    goToSlide(currentSlide+1);
+//}
+//
+//function previousSlide() {
+//    goToSlide(currentSlide-1);
+//}
+//
+//function goToSlide(n) {
+//    slides[currentSlide].className = 'slide';
+//    currentSlide = (n+slides.length)%slides.length;
+//    slides[currentSlide].className = 'slide showing';
+//}
+//
+//
+//var next = document.getElementById('next');
+//var previous = document.getElementById('previous');
+//
+//next.onclick = function() {
+//    pauseSlideshow();
+//    nextSlide();
+//};
+//previous.onclick = function() {
+//    pauseSlideshow();
+//    previousSlide();
+//};
+//
+//var next = document.getElementById('next');
+//var previous = document.getElementById('previous');
+//
+//next.onclick = function() {
+//    pauseSlideshow();
+//    nextSlide();
+//};
+//previous.onclick = function() {
+//    pauseSlideshow();
+//    previousSlide();
+//};
 
